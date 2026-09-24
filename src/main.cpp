a5a17e443d6102e7a75fea87d3c4dd90c5b8bfcb
Lohkdesgds <lohk.lohkdesgds@gmail.com>
2026-09-24 20:48:57 -0300

    Making bot use library instead of compiling the whole lib by hand

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,35 @@
 #include <dpp/dpp.h>
+#include <dpp/nlohmann/json.hpp>
 #include <cstdlib>
 
 int main() {
-	dpp::cluster bot(std::getenv("BOT_TOKEN"));
+    std::cout << "Starting bot..." << std::endl;
+    const auto token = std::getenv("BOT_TOKEN");
+    if (!token) {
+        std::cout << "Invalid token. Set env variable BOT_TOKEN and try again." << std::endl;
+        return 1;
+    }
+
+	dpp::cluster bot(token);
 
     bot.set_websocket_protocol(dpp::ws_etf);
 
-    bot.on_log([](const dpp::log_t& log) {
-        switch(log.severity) {
-        case dpp::loglevel::ll_info:
-            std::cout << "[I] " << log.message << std::endl;
-            break;
-        case dpp::loglevel::ll_warning:
-            std::cout << "[W] " << log.message << std::endl;
-            break;
-        case dpp::loglevel::ll_error:
-            std::cout << "[E] " << log.message << std::endl;
-            break;
-        case dpp::loglevel::ll_critical:
-            std::cout << "[C] " << log.message << std::endl;
-            break;
-        }
-    });
+    std::cout << "Setting up log hook..." << std::endl;
+    bot.on_log(dpp::utility::cout_logger());
 
+    std::cout << "Setting up slash command hook..." << std::endl;
 	bot.on_slashcommand([](auto event) {
 		if (event.command.get_command_name() == "ping") {
 			event.reply("Pong!");
 		}
 	});
 
+    std::cout << "Setting up guild create hook..." << std::endl;
     bot.on_guild_create([&bot](dpp::guild_create_t event) {
         std::cout << "Joined server " << event.created.name << std::endl;
     });
 
+    std::cout << "Setting up on ready hook..." << std::endl;
 	bot.on_ready([&bot](dpp::ready_t event) {
 		if (dpp::run_once<struct register_bot_commands>()) {
 			bot.global_command_create(
@@ -41,6 +38,7 @@ int main() {
 		}
 	});
 
+    std::cout << "Start!" << std::endl;
 	bot.start(dpp::st_wait);
 	return 0;
 }
