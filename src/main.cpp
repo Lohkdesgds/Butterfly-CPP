9ccb52e181321ca4df1adf3ca5a8985bb5e12124
Lohkdesgds <lohk.lohkdesgds@gmail.com>
2026-09-24 20:12:42 -0300

    Testing cpp first

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <dpp/dpp.h>
+#include <cstdlib>
+
+int main() {
+	dpp::cluster bot(std::getenv("BOT_TOKEN"));
+
+	bot.on_slashcommand([](auto event) {
+		if (event.command.get_command_name() == "ping") {
+			event.reply("Pong!");
+		}
+	});
+
+    bot.on_guild_create([&bot](dpp::guild_create_t event) {
+        std::cout << "Joined server " << event.created.name << std::endl;
+    });
+
+	bot.on_ready([&bot](dpp::ready_t event) {
+		if (dpp::run_once<struct register_bot_commands>()) {
+			bot.global_command_create(
+				dpp::slashcommand("ping", "Ping pong!", bot.me.id)
+			);
+		}
+	});
+
+	bot.start(dpp::st_wait);
+	return 0;
+}
